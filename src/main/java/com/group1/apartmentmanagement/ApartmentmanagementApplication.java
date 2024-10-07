220605de6ea54abc0c05a3dd89f749f850ac76b4
Tuan Kiet <Tuankie102@gmail.com>
2024-10-07 11:04:42 +0700

    initial

---
+++ src/main/java/com/group1/apartmentmanagement/ApartmentmanagementApplication.java
@@ -0,0 +1,14 @@
+package com.group1.apartmentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class ApartmentmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApartmentmanagementApplication.class, args);
+	}
+
+}
