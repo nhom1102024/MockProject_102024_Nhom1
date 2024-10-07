7fd2726f7ab752df73314cbaa47720f7ae0625b9
HoangVanThong <hoanggvannthongg@gmail.com>
2024-10-07 15:12:12 +0700

    Initial commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,26 +1,22 @@
-import React from "react";
-import { useFormik } from "formik";
-import * as Yup from "yup";
+import React, { useState } from "react";
 
 import "./Login.css";
 
 const Login = () => {
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      password: "",
-    },
-    validationSchema: Yup.object({
-      email: Yup.string().email("Invalid email address").required("Required"),
-      password: Yup.string()
-        .min(6, "Password must be at least 6 characters")
-        .required("Required"),
-    }),
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-      history.push("/dashboard");
-    },
-  });
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const hardCodedEmail = "hoangvanthong@gmail.com";
+  const hardCodedPassword = "123456";
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if (email === hardCodedEmail && password === hardCodedPassword) {
+      alert("Log in successfully!");
+    } else {
+      setError("Email or password is incorrect");
+    }
+  };
 
   return (
     <div className="wrapper">
@@ -29,40 +25,33 @@ const Login = () => {
           <h2 className="heading">Xin Chào! Bắt đầu nào</h2>
           <h3 className="title">Đăng nhập để tiếp tục</h3>
         </div>
-        <form onSubmit={formik.handleSubmit} className="form-control">
+        <form onSubmit={handleLogin} className="form-control">
           <div className="inputGroup">
             <input
               id="email"
               name="email"
               type="email"
+              required
               placeholder="Username"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.email}
+              onChange={(e) => setEmail(e.target.value)}
               className="input"
             />
-            {formik.touched.email && formik.errors.email ? (
-              <div className="error">{formik.errors.email}</div>
-            ) : null}
           </div>
           <div className="inputGroup">
             <input
               id="password"
               name="password"
               type="password"
+              required
               placeholder="Password"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.password}
+              onChange={(e) => setPassword(e.target.value)}
               className="input"
             />
-            {formik.touched.password && formik.errors.password ? (
-              <div className="error">{formik.errors.password}</div>
-            ) : null}
           </div>
           <button type="submit" className="button">
             Login
           </button>
+          {error && <p className="error">{error}</p>}
         </form>
         <div className="login-footer">
           <a href="#" className="forgot-pass">
