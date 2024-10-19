26e5c37dc72f70686610239f9fbe900bb9f1a8f9
Đinh Công Thành <cngthnh22112002@gmail.com>
2024-10-19 19:45:24 +0700

    chore: change Role to UserRole

--- backend/src/main/java/com/lease_master/consts/UserRole.java
+++ backend/src/main/java/com/lease_master/consts/UserRole.java
@@ -1,6 +1,6 @@
 package com.lease_master.consts;
 
-public class Role {
+public class UserRole {
 	public static final String USER = "User";
 	public static final String ADMIN = "Admin";
 	public static final String EMPLOYEE = "Employee";
