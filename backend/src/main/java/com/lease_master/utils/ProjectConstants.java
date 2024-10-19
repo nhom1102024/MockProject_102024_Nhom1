953fdf64d467708e06f55401adfc96e1327892f3
Đinh Công Thành <cngthnh22112002@gmail.com>
2024-10-19 19:44:50 +0700

    chore: update language

--- backend/src/main/java/com/lease_master/utils/ProjectConstants.java
+++ backend/src/main/java/com/lease_master/utils/ProjectConstants.java
@@ -9,7 +9,7 @@ public final class ProjectConstants {
 
 	public static final String DEFAULT_ENCODING = "UTF-8";
 
-	public static final Locale TURKISH_LOCALE = new Locale.Builder().setLanguage("tr").setRegion("TR").build();
+	public static final Locale ENGLISH_LOCALE = new Locale.Builder().setLanguage("es").setRegion("ES").build();
 
 	private ProjectConstants() {
 
