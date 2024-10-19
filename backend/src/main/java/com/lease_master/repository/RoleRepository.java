e8664aedc9c700234c7e4affc8df0d15af0c3c7e
Đinh Công Thành <cngthnh22112002@gmail.com>
2024-10-19 19:51:24 +0700

    feat: implement Role service

--- backend/src/main/java/com/lease_master/repository/RoleRepository.java
+++ backend/src/main/java/com/lease_master/repository/RoleRepository.java
@@ -4,6 +4,9 @@ import com.lease_master.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface RoleRepository extends JpaRepository<Role, Long> {
+public interface RoleRepository extends JpaRepository<Role, Integer> {
 
+    boolean existsByRoleName(String roleName);
+
+    Role findByRoleName(String roleName);
 }
